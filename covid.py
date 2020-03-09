52877832105c062f1ff3f05e0dc5267b6f0354c7
Matteo Monchiero <matteo@streamlit.io>
2020-03-08 18:08:12 -0700

    mods

--- covid.py
+++ covid.py
@@ -2,21 +2,25 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
+st.title("Track Covid-19 over Time")
+
 URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 data = pd.read_csv(URL)
+st.markdown("Whole dataset from https://github.com/CSSEGISandData/COVID-19:")
 st.write(data)
 
 regions = data["Country/Region"].unique()
-region = st.selectbox("Country/Region", regions)
+region = st.selectbox("Select a Country/Region", regions)
 data = data[data["Country/Region"] == region]
-st.markdown("By Region:")
+
+st.markdown("Filtered by Country/Region:")
 st.write(data)
 
 search_value = st.text_input("Search Province/State")
-include_na = st.checkbox("Include NA", False)
+include_na = st.checkbox("Include NA", True)
 data = data[data["Province/State"].str.contains(search_value).fillna(include_na)]
 
-st.markdown("By Search:")
+st.markdown("Filtered by Province/State search:")
 st.write(data)
 
 data = data.iloc[:, 4:]
@@ -26,6 +30,7 @@ data = data.agg("sum").reset_index()
 #data = data.T.reset_index()
 data.columns = ['day', 'value']
 
+st.markdown("### Total Confirmed Cases")
 chart = (
     alt.Chart(data)
     .mark_line()
